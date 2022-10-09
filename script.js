f72ba8c919c9a36e19f3d068113001db596bbbd4
top_004 <locinbekerkinov38@gmail.com>
2022-10-09 11:11:57 +0500

    task-4

---
+++ script.js
@@ -0,0 +1,41 @@
+var num = +prompt('Введите месецы');
+switch (num) {
+    case 1: 
+    alert(' Первый месяц январь')
+    break
+    case 2: 
+    alert(' Второй месяц февраль')
+    break
+    case 3: 
+     alert(' Третий месяц март')
+     break
+  case 4: 
+   alert('Четвёртый месяц март')
+   break
+  case 5: 
+   alert(' Пятый месяц май')
+   break
+  case 6: 
+   alert(' Шестой месяц июнь')
+   break
+  case 7: 
+   alert(' Седьмой месяц июль')
+   break
+  case 8: 
+   alert(' Восьмой месяц август')
+   break
+  case 9: 
+   alert(' Девятый месяц сентябрь')
+   break
+  case 10: 
+   alert(' Десятый месяц октябрь')
+   break
+  case 11: 
+   alert(' Одинадцатый месяц ноябрь')
+   break
+  case 12: 
+   alert(' Двинадцатый месяц декабрь')
+   break
+   default:
+     alert('Такого месеца нету')
+ }
